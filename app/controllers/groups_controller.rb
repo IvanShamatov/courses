f8fbc7884a749889ed0b72b28b995090abf1dee9
odinsy <odidoit@gmail.com>
2015-06-13 20:17:39 +0300

    added some fix to controllers and views

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,74 +1,55 @@
 class GroupsController < ApplicationController
+  before_action :set_course
   before_action :set_group, only: [:show, :edit, :update, :destroy]
 
-  # GET /groups
-  # GET /groups.json
   def index
-    @groups = Group.all
+    @groups = @course.groups.all
   end
 
-  # GET /groups/1
-  # GET /groups/1.json
   def show
   end
 
-  # GET /groups/new
   def new
-    @group = Group.new
+    @group = @course.groups.new
   end
 
-  # GET /groups/1/edit
   def edit
   end
 
-  # POST /groups
-  # POST /groups.json
   def create
-    @group = Group.new(group_params)
-
-    respond_to do |format|
-      if @group.save
-        format.html { redirect_to @group, notice: 'Group was successfully created.' }
-        format.json { render :show, status: :created, location: @group }
-      else
-        format.html { render :new }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
+    @group = @course.groups.create(group_params)
+    if @group.errors.empty?
+      redirect_to course_groups_path
+    else
+      render 'new'
     end
   end
 
-  # PATCH/PUT /groups/1
-  # PATCH/PUT /groups/1.json
   def update
-    respond_to do |format|
-      if @group.update(group_params)
-        format.html { redirect_to @group, notice: 'Group was successfully updated.' }
-        format.json { render :show, status: :ok, location: @group }
-      else
-        format.html { render :edit }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
+    @course.groups.update_attributes(group_params)
+    if @course.errors.empty?
+      redirect_to course_groups_path
+    else
+      render 'edit'
     end
   end
 
-  # DELETE /groups/1
-  # DELETE /groups/1.json
   def destroy
     @group.destroy
-    respond_to do |format|
-      format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to course_groups_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
+    def set_course
+      @course = Course.find(params[:course_id])
+    end
+
     def set_group
       @group = Group.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def group_params
-      params[:group]
+      params.require(:group).permit(:start_date, :end_date, :weekday, :start_time, :end_time)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150613102955) do
+ActiveRecord::Schema.define(version: 20150613143450) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150613102955) do
     t.time     "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "course_id"
   end
 
   create_table "groups_users", id: false, force: :cascade do |t|
